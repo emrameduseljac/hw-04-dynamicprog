8adfbdf669259074b089c1d9db9d7f68f981fae5
AanthonyRusso <157141848+AanthonyRusso@users.noreply.github.com>
2024-10-05 18:39:41 -0500

    spiderman recursive working, no dynamic

--- spiderman.py
+++ spiderman.py
@@ -1,1 +1,29 @@
-# solution for Spiderman's Workout
+def workout(distance, intList, path, numsMoves):
+    if (len(path) == numsMoves): # base case: if the length of the path is equal to the number of moves
+        if (distance == 0): # only valid if the length is 0
+            return path
+        else:
+            return "IMPOSSIBLE"
+    if (distance < 0):
+        return "IMPOSSIBLE"
+    else:
+        result = workout(distance + intList[len(path)], intList, path + "U", numsMoves)
+        if (result != "IMPOSSIBLE"):
+            return result   
+        if (distance - intList[len(path)] >= 0): # checks if distance would go below 0
+            result = workout(distance - intList[len(path)], intList, path + "D", numsMoves)
+            if (result != "IMPOSSIBLE"):
+                return result
+
+    return "IMPOSSIBLE"
+
+tests = int(input())
+
+for i in range (0,tests):
+    numberMoves = int(input())
+    moves = input()
+    movesList = [int(x) for x in moves.split()]
+
+
+    result = workout(0, movesList, "",numberMoves )
+    print(result)
