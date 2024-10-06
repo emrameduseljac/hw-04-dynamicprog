0e61fc7ddad4e4ba8eab96be14336e6d90b419c9
teamomiamigo <nmuthukumar@slu.edu>
2024-10-06 18:38:28 -0500

    changes to spiderman for it to pass all test cases and be fixed

--- spiderman.py
+++ spiderman.py
@@ -1,44 +1,56 @@
-def printTable(table):
-    for j in range (len(table)):     #this was for testing
-        print("Row ", j+1)
-        for i in range (len(table[j])):       
-            if table[j][i] != False:
-                print(i) 
-
 def spiderman(climbList, size):
-    size = len(climbList)
-    maxi = (sum(climbList))//2
-    table = [[False] * (maxi + 1) for i in range(size + 1)]
-    table[0][0] = True
+    total = sum(climbList)
+    maxi = total
+    inf = float('inf')
+
+
+    table = [[inf] * (maxi + 1) for _ in range(size + 1)]
+    decisions = [[0] * (maxi + 1) for _ in range(size + 1)]
+
+    table[0][0] = 0
+
+    if total % 2 == 1:
+        return "IMPOSSIBLE"
 
     for i in range(1, size + 1):
         dDistance = climbList[i - 1]
         for j in range(maxi + 1):
-            if table[i - 1][j] != False:
-                if j + dDistance <= maxi:
-                    table[i][j + dDistance] = True #fills out table of valid places
-                if j - dDistance >= 0:
-                    table[i][j - dDistance] = True #fills out table of valid places
-    if table[size][0] != True: #if touching the ground isnt possible it returns impossible
+            if table[i - 1][j] != inf:
+                new_h = j + dDistance
+                if new_h <= maxi:
+                    new_max_h = max(table[i-1][j], new_h)
+                    if new_max_h < table[i][new_h]:
+                        table[i][new_h] = new_max_h
+                        decisions[i][new_h] = 1
+                new_h = j - dDistance
+                if new_h >= 0:
+                    new_max_h = table[i-1][j]
+                    if new_max_h < table[i][new_h]:
+                        table[i][new_h] = new_max_h
+                        decisions[i][new_h] = -1
+                
+    if table[size][0] == inf:
         return "IMPOSSIBLE"
-    else: # backtracks to find path
-        path = ""
-        distance = 0
-        for i in range(size, 0, -1):
-            jump = climbList[i - 1]
-            if (distance - jump >= 0 and table[i - 1][distance - jump]): #tries to stay as close to the ground as possible while backtracking solutions
-                path = 'U' + path
-                distance -= jump
-            else:
-                path = 'D' + path
-                distance += jump
-            print(distance)
+    
+    # backtracks to find path
+    path = ""
+    h = 0
+    for i in range(size, 0, -1):
+        decision = decisions[i][h]
+        if decision == 1:
+            path = "U" + path
+            h -= climbList[i - 1]
+        elif decision == -1:
+            path = "D" + path
+            h += climbList[i - 1]
+        else:
+            return "IMPOSSIBLE"
 
     return path
+
 tests = int(input())
-for i in range(tests):
+for _ in range(tests):
     size = int(input())
     climbS = input()
     climbList = [int(num) for num in climbS.split()]
     print(spiderman(climbList, size))
-    
