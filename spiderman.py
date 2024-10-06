dc5f694f44f05a73822134be2c0211d34ba0a2d1
AanthonyRusso <157141848+AanthonyRusso@users.noreply.github.com>
2024-10-06 15:10:03 -0500

    Update spiderman.py

--- spiderman.py
+++ spiderman.py
@@ -6,22 +6,39 @@ def printTable(table):
                 print(i) 
 
 def spiderman(climbList, size):
-    max = sum(climbList)
-    table = [[False] * (max + 1) for i in range(size + 1)]
-    table[0][0] = ""
+    size = len(climbList)
+    maxi = (sum(climbList))//2
+    table = [[False] * (maxi + 1) for i in range(size + 1)]
+    table[0][0] = True
 
     for i in range(1, size + 1):
         dDistance = climbList[i - 1]
-        for j in range(max + 1):
+        for j in range(maxi + 1):
             if table[i - 1][j] != False:
-                if j + dDistance <= max:
+                if j + dDistance <= maxi:
                     table[i][j + dDistance] = True #fills out table of valid places
                 if j - dDistance >= 0:
                     table[i][j - dDistance] = True #fills out table of valid places
-    printTable(table)
     if table[size][0] != True: #if touching the ground isnt possible it returns impossible
         return "IMPOSSIBLE"
-    else:
-        return "path" #maybe use some kind of backtracking to minimize height?
+    else: # backtracks to find path
+        path = ""
+        distance = 0
+        for i in range(size, 0, -1):
+            jump = climbList[i - 1]
+            if (distance - jump >= 0 and table[i - 1][distance - jump]): #tries to stay as close to the ground as possible while backtracking solutions
+                path = 'U' + path
+                distance -= jump
+            else:
+                path = 'D' + path
+                distance += jump
+            print(distance)
 
-print(spiderman([20,20,20,20], 4))
+    return path
+tests = int(input())
+for i in range(tests):
+    size = int(input())
+    climbS = input()
+    climbList = [int(num) for num in climbS.split()]
+    print(spiderman(climbList, size))
+    
