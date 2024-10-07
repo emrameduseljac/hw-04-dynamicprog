39a4965024da7169b80af676172ad47a1f2ceaab
Burhan Khan <burhankhan@Burhans-MacBook-Air.local>
2024-10-07 05:18:52 -0500

    Trainsorting passes all test cases on Kattis

--- trainsorting.py
+++ trainsorting.py
@@ -11,17 +11,18 @@ def trainsorting(numOfCars, weightOfCars):
         for prev_car in range(i):
             if weightOfCars[curr_car] > weightOfCars[prev_car]:
                 # if weight of current car is heavier than weight of a previous car, add 1 to length of current car
-                increasing[curr_car] = increasing[prev_car]+1       
+                increasing[curr_car] = max(increasing[curr_car], increasing[prev_car]+1)      
     # start from last car to compare with future car(s)
     for curr_car in reversed(range(numOfCars)):
         for future_car in range(curr_car + 1, numOfCars):
             if weightOfCars[curr_car] > weightOfCars[future_car]:
                 # if weight of current car greater than weight of future car, add 1 to length of current car
-                decreasing[curr_car] = decreasing[future_car]+1
+                decreasing[curr_car] = max(decreasing[curr_car], decreasing[future_car]+1)
+
     # outputs number of cars in the longest train that can be made given the restrictions
     max_length = 0
     for i in range(numOfCars):
-        max_length = increasing[i] + decreasing[i] - 1
+        max_length = max(max_length, increasing[i] + decreasing[i] - 1)
     return max_length
     
 # input
